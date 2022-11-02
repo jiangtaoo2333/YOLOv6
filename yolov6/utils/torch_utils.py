2eaa48b9d0d6ba222e8ce5e34a1a0729927ff55e
姜涛 <jiangtao2@streamax.com>
2022-11-02 17:45:18 +0800

    修改torch_distributed_zero_first适配torch1.7

--- yolov6/utils/torch_utils.py
+++ yolov6/utils/torch_utils.py
@@ -22,10 +22,10 @@ def torch_distributed_zero_first(local_rank: int):
     Decorator to make all processes in distributed training wait for each local_master to do something.
     """
     if local_rank not in [-1, 0]:
-        dist.barrier(device_ids=[local_rank])
+        torch.distributed.barrier()
     yield
     if local_rank == 0:
-        dist.barrier(device_ids=[0])
+        torch.distributed.barrier()
 
 
 def time_sync():
